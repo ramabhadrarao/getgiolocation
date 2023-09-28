90c4288e7e68a98d7cad5d70ff9557f3998e0199
maddu rama bhadra rao <ramabhadrarao.maddu@yahoo.com>
2023-09-28 17:05:04 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -15,7 +15,6 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS giopositions (id INT AUTO_INCREMENT
 
 # Streamlit app
 st.title("Get Location Coordinates")
-geolocator = Nominatim(user_agent="my_geo_app")
 
 # Function to get coordinates
 def get_coordinates():
@@ -26,6 +25,27 @@ def get_coordinates():
         st.error(f"Error occurred: {e}")
         return None, None
 
+# Function to calculate haversine distance
+def haversine(lat1, lon1, lat2, lon2):
+    # Convert latitude and longitude from degrees to radians
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    # Radius of the Earth in kilometers
+    R = 6371.0
+
+    # Calculate the distance
+    distance = R * c
+
+    return distance
 
 # Button to trigger location retrieval
 button_clicked = st.button("Get Location")
@@ -53,8 +73,9 @@ for i, (lat1, lon1) in enumerate(coordinates):
     G.add_node(i, pos=(lat1, lon1))
     for j, (lat2, lon2) in enumerate(coordinates):
         if i != j:
-            distance = math.dist((lat1, lon1), (lat2, lon2))
+            distance = haversine(lat1, lon1, lat2, lon2)
             G.add_edge(i, j, weight=distance)
+
 # Draw network diagram
 fig, ax = plt.subplots()
 pos = nx.get_node_attributes(G, 'pos')
