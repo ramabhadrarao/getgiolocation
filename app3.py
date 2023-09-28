d23222c283421eba36d666b676865d85c211d8a5
maddu rama bhadra rao <ramabhadrarao.maddu@yahoo.com>
2023-09-28 15:34:14 +0530

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import geocoder
+import math
+import networkx as nx
+import matplotlib.pyplot as plt
+import mysql.connector
+
+# Connect to MySQL
+mydb = mysql.connector.connect(**st.secrets["mysql"])
+
+# Create a cursor object
+mycursor = mydb.cursor()
+
+# Streamlit app
+st.title("Get Location Coordinates")
+
+# Function to get coordinates
+def get_coordinates():
+    g = geocoder.ip('me')
+    return g.lat, g.lng
+
+# Button to trigger location retrieval
+button_clicked = st.button("Get Location")
+
+if button_clicked:
+    latitude, longitude = get_coordinates()
+    st.write(f"Latitude: {latitude}")
+    st.write(f"Longitude: {longitude}")
+
+    # Store the coordinates in the MySQL table
+    sql = "INSERT INTO giopositions (latitude, longitude) VALUES (%s, %s)"
+    val = (latitude, longitude)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    st.write("Coordinates saved to MySQL database")
+
+# Read coordinates from MySQL
+mycursor.execute("SELECT * FROM giopositions")
+coordinates = mycursor.fetchall()
+
+# Calculate distances and create network diagram
+G = nx.Graph()
+for i, (lat1, lon1) in enumerate(coordinates):
+    G.add_node(i, pos=(lat1, lon1))
+    for j, (lat2, lon2) in enumerate(coordinates):
+        if i != j:
+            distance = math.dist((lat1, lon1), (lat2, lon2))
+            G.add_edge(i, j, weight=distance)
+
+# Draw network diagram
+fig, ax = plt.subplots()
+pos = nx.get_node_attributes(G, 'pos')
+nx.draw(G, pos, with_labels=True, ax=ax)
+labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, ax=ax)
+st.pyplot(fig)
+
+# Close MySQL connection
+mycursor.close()
+mydb.close()
